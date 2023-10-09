5f1ae335e9a6a51410cbac28c313ab553c822479
davidslayin <dave191994@gmail.com>
2023-10-10 01:59:53 +0300

    Add files via upload

---
+++ 100-clean_web_static.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+Fabric script methods:
+    do_pack: packs web_static/ files into .tgz archive
+    do_deploy: deploys archive to webservers
+    deploy: do_packs && do_deploys
+    do_clean(n=0): removes old versions and keeps n (or 1) newest versions only
+Usage:
+    fab -f 100-clean_web_static.py do_clean:number=2 -i my_ssh_private_key -u
+    ubuntu > /dev/null 2>&1
+"""
+from fabric.api import local, env, put, run
+from datetime import datetime
+import os.path
+env.hosts = ['3.238.230.209', '18.205.104.232']
+
+
+def do_pack():
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
+
+
+def do_deploy(archive_path):
+    """
+    Deploy archive to web server
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    success = do_deploy(archive_path)
+    return success
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+    files = local("ls -1t versions", capture=True)
+    file_names = files.split("\n")
+    n = int(number)
+    if n in (0, 1):
+        n = 1
+    for i in file_names[n:]:
+        local("rm versions/{}".format(i))
+    dir_server = run("ls -1t /data/web_static/releases")
+    dir_server_names = dir_server.split("\n")
+    for i in dir_server_names[n:]:
+        if i is 'test':
+            continue
+        run("rm -rf /data/web_static/releases/{}"
+            .format(i))
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Fabric script methods:
+    do_pack: packs web_static/ files into .tgz archive
+    do_deploy: deploys archive to webservers
+    deploy: do_packs && do_deploys
+Usage:
+    fab -f 3-deploy_web_static.py deploy -i my_ssh_private_key -u ubuntu
+"""
+from fabric.api import local, env, put, run
+from datetime import datetime
+import os.path
+env.hosts = ['3.238.230.209', '18.205.104.232']
+
+
+def do_pack():
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -czvf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
+
+
+def do_deploy(archive_path):
+    """
+    Deploy archive to web server
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    success = do_deploy(archive_path)
+    return success
